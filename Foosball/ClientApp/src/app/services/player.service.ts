91e03056177ef208b74af9da45cdb728cedda2c1
Mech0z <2316684+Mech0z@users.noreply.github.com>
2018-12-16 00:20:11 +0100

    Fixes

--- Foosball/ClientApp/src/app/services/player.service.ts
+++ Foosball/ClientApp/src/app/services/player.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { User } from '../models/index';
-
 @Injectable()
 export class PlayerService {
   constructor(private http: HttpClient) { }
 
   requestNewPassword(email: string) {
     return this.http.get('https://foosballapi.azurewebsites.net' + '/api/Player/RequestPassword/?email=' + email)
-      .map(response => { return response; });
   }
 }
