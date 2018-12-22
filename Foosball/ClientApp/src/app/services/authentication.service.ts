ae3e650cc8154bfbe3ead0055d686268c8c5d7d6
Martin Søberg <maso@seges.dk>
2018-12-22 21:21:29 +0100

    ng cli upgrade

--- Foosball/ClientApp/src/app/services/authentication.service.ts
+++ Foosball/ClientApp/src/app/services/authentication.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
+import { from } from 'rxjs';
+import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 import { LoginRequest } from '../models/LoginRequest';
 
 @Injectable()
