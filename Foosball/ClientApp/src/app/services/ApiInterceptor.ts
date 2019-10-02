c20309056dce0ac9d0cf20c9fef324eee4dcd1e8
Mech0z <2316684+Mech0z@users.noreply.github.com>
2019-10-02 19:50:32 +0200

    small api fix

--- Foosball/ClientApp/src/app/services/ApiInterceptor.ts
+++ Foosball/ClientApp/src/app/services/ApiInterceptor.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      const baseUrl = 'http://betafoosballapi.azurewebsites.net';
+      const baseUrl = 'https://betafoosballapi.azurewebsites.net';
         const apiReq = req.clone({ url: `${baseUrl}${req.url}` });
         return next.handle(apiReq);
     }
