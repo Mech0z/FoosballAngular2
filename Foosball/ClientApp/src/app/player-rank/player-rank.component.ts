6643226f55e85a787112f1088d653254e963c878
Peter Fjordside Rasmussen <pera@seges.dk>
2020-02-12 20:39:08 +0100

    New feature: Pull down to refresh, hide fab on scroll to bottom
    and back btn on player-rank

--- Foosball/ClientApp/src/app/player-rank/player-rank.component.ts
+++ Foosball/ClientApp/src/app/player-rank/player-rank.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PlayerService } from '../services/player.service';
 import { GraphModel } from '../models/GraphModel';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-player-rank',
@@ -10,91 +11,95 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class PlayerRankComponent implements OnInit, OnDestroy {
 
-    multi: any[];
-    view: any[] = [1000, 600];
-    datasource: GraphModel[];
-    // options
-    legend = true;
-    showLabels = true;
-    animations = true;
-    xAxis = true;
-    yAxis = true;
-    showYAxisLabel = true;
-    showXAxisLabel = true;
-    xAxisLabel = 'Date';
-    yAxisLabel = 'Rating';
-    timeline = true;
-    autoScale = true;
+  multi: any[];
+  view: any[] = [1000, 600];
+  datasource: GraphModel[];
+  // options
+  legend = true;
+  showLabels = true;
+  animations = true;
+  xAxis = true;
+  yAxis = true;
+  showYAxisLabel = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Date';
+  yAxisLabel = 'Rating';
+  timeline = true;
+  autoScale = true;
 
-    colorScheme = {
-      domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
-    };
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
 
-    constructor(
-      private playerService: PlayerService,
-      private router: Router,
-      private route: ActivatedRoute) {
-      }
+  constructor(
+    private playerService: PlayerService,
+    private location: Location,
+    private route: ActivatedRoute) {
+  }
 
-    ngOnInit(): void {
-      // localhost:4200/player-rank/madsskipper@gmail.com/The%20Hip%20Season%20%2816%29
-      const email = this.route.snapshot.paramMap.get('email');
-      const season = this.route.snapshot.paramMap.get('season');
+  ngOnInit(): void {
+    // localhost:4200/player-rank/madsskipper@gmail.com/The%20Hip%20Season%20%2816%29
+    const email = this.route.snapshot.paramMap.get('email');
+    const season = this.route.snapshot.paramMap.get('season');
 
-      if(email === null) {
-        this.initAllPlayers(season);
-      } else {
-        this.initSinglePlayer(email, season);
-      }
+    if (email === null) {
+      this.initAllPlayers(season);
+    } else {
+      this.initSinglePlayer(email, season);
     }
+  }
 
-    initSinglePlayer(email: string, season: string) {
-      this.playerService.getPlayerRank(email, season).subscribe(result => {
-        const data: GraphModel[] = [{name: email, series: []}];
+  initSinglePlayer(email: string, season: string) {
+    this.playerService.getPlayerRank(email, season).subscribe(result => {
+      const data: GraphModel[] = [{ name: email, series: [] }];
 
-        result.rankPlots.forEach(plot => {
-          data[0].series.push({name: plot.date, value: plot.eloRating});
+      result.rankPlots.forEach(plot => {
+        data[0].series.push({ name: plot.date, value: plot.eloRating });
+      });
+
+      let array_name;
+      array_name = [data];
+
+      this.datasource = data;
+    }, error => {
+      console.error(error);
+    });
+  }
+
+  initAllPlayers(season: string) {
+    this.playerService.getPlayersRanks(season).subscribe(result => {
+      const data: GraphModel[] = [];
+      result.forEach(element => {
+        const player: GraphModel = { name: element.email, series: [] };
+        element.rankPlots.forEach(plot => {
+          player.series.push({ name: plot.date, value: plot.eloRating });
         });
+        data.push(player);
+      });
 
-        let array_name;
-        array_name = [data];
+      this.datasource = data;
+    }, error => {
+      console.error(error);
+    });
+  }
 
-        this.datasource = data;
-     }, error => {
-       console.error(error);
-     });
-    }
+  onSelect(data): void {
+    // console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
 
-    initAllPlayers(season: string) {
-      this.playerService.getPlayersRanks(season).subscribe(result => {
-              const data: GraphModel[] = [];
-              result.forEach(element => {
-                const player: GraphModel = {name: element.email, series: []};
-                element.rankPlots.forEach(plot => {
-                  player.series.push({name: plot.date, value: plot.eloRating});
-               });
-               data.push(player);
-              });
+  onActivate(data): void {
+    // console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
 
-               this.datasource = data;
-            }, error => {
-              console.error(error);
-            });
-    }
+  onDeactivate(data): void {
+    // console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 
-    onSelect(data): void {
-      // console.log('Item clicked', JSON.parse(JSON.stringify(data)));
-    }
+  ngOnDestroy(): void {
+    // this.subs.forEach(s => s.unsubscribe());
+  }
 
-    onActivate(data): void {
-      // console.log('Activate', JSON.parse(JSON.stringify(data)));
-    }
-
-    onDeactivate(data): void {
-      // console.log('Deactivate', JSON.parse(JSON.stringify(data)));
-    }
-
-    ngOnDestroy(): void {
-      // this.subs.forEach(s => s.unsubscribe());
-    }
+  goBack() {
+    this.location.back();
+  }
 }
--- Foosball/ClientApp/src/app/views/responsive-overview/responsive-overview.component.ts
+++ Foosball/ClientApp/src/app/views/responsive-overview/responsive-overview.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import { HeadersService } from '../../services/headers.service';
 import { AuthenticationService } from '../../services/authentication.service';
 import { ThemeService } from '../../shared/theme.service';
-import { Observable } from 'rxjs';
+import { Observable, merge, fromEvent, Subscription, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-responsive-overview',
   templateUrl: './responsive-overview.component.html',
   styleUrls: ['./responsive-overview.component.scss']
 })
-export class ResponsiveOverviewComponent implements OnInit {
+export class ResponsiveOverviewComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('scrollContainer') scrollContainer: ElementRef;
+  private subs = new Subscription();
   darkTheme$: Observable<boolean>;
+  fabVisible = new Subject<boolean>();
+  fabVisible$ = this.fabVisible.asObservable();
+  refreshVisible = new Subject<boolean>();
+  refreshVisible$ = this.refreshVisible.asObservable();
   isAdmin = false;
+  startOffset: number;
+  startY: number;
+  lastY: number;
+  endY: number;
 
   constructor(
     private themeService: ThemeService,
@@ -30,5 +40,45 @@ export class ResponsiveOverviewComponent implements OnInit {
         window.location.reload();
       });
     }
+  }
+
+  ngAfterViewInit() {
+    this.setupScrollSubscribtion();
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+
+  private setupScrollSubscribtion() {
+    const mouseDown = fromEvent(this.scrollContainer.nativeElement, 'touchstart');
+    const mouseUp = fromEvent(this.scrollContainer.nativeElement, 'touchend');
+    const mouseMove = fromEvent(this.scrollContainer.nativeElement, 'touchmove');
+    const scrollEnd = fromEvent(this.scrollContainer.nativeElement, 'scroll');
+
+    this.subs.add(mouseDown.subscribe((e: any) => {
+      this.startOffset = this.scrollContainer.nativeElement.scrollTop;
+      this.startY = e.changedTouches[0].clientY;
+    }));
+    this.subs.add(mouseUp.subscribe((e: any) => {
+      this.endY = e.changedTouches[0].clientY;
+      const scrollOverTop = this.endY - this.startY - this.startOffset;
+      if (scrollOverTop > 200) {
+        location.reload();
+      } else {
+        this.refreshVisible.next(false);
+      }
+    }));
+    this.subs.add(mouseMove.subscribe((e: any) => {
+      const currentY = e.changedTouches[0].clientY;
+      this.refreshVisible.next((currentY - this.startY - this.startOffset > 0) && this.lastY < currentY);
+      this.lastY = currentY;
+    }));
+    this.subs.add(scrollEnd.subscribe((e: any) => {
+      const scrollElement = this.scrollContainer.nativeElement;
+      const offset = scrollElement.offsetHeight - scrollElement.scrollTop + scrollElement.offsetHeight;
+      this.fabVisible.next(offset < 100);
+    }));
+
   }
 }
